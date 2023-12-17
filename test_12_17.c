4de29a4ef7bd96c1c4e81b8acb8b63c163485e36
ricardomlu021 <15364707012@163.com>
2023-12-17 23:09:17 +0800

    2023-12-17学习

---
+++ test_12_17.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()//主函数-程序的入口-有且只有一个
+{
+	//printf("hello world\n");
+
+	//char ch = 'A';
+	//printf("%c\n",ch);//%c-打印字符格式的数据
+	
+	//int age = 23;
+	//printf("%d\n",age);//%d-打印整型十进制数据
+
+	//float f = 1.5;
+	//printf("%f\n",f);
+
+	//double d = 2.46;
+	//printf("%lf\n",d);
+
+	//printf("%d\n",sizeof(char));//
+	//printf("%d\n",sizeof(int));//
+	//printf("%d\n",sizeof(short));//
+	//printf("%d\n",sizeof(long));//
+	//printf("%d\n",sizeof(long long));//
+	//printf("%d\n",sizeof(float));//
+	//printf("%d\n",sizeof(double));//
+	return 0;
+}
+
+//int-表示整型
+//\n-表示换行
+//用别人的东西，先打招呼：#include <>
+//stdio  std-standard  i-inprt  o-output  输入输出
+//char - 字符数据类型
+//int - 整型
+//short int - 短整型
+//long - 长整型
+//float - 单精度浮点数
+//double - 双精度浮点数
+
